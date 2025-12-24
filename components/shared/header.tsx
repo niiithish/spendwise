53be6eefd6090f4632702fc796254b556f5b26d1
Nithish Gowda <niiithish@gmail.com>
2025-12-24 19:22:30 +0530

    Added a header

---
+++ components/shared/header.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Switch } from "../ui/switch";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Sun01Icon, Moon02Icon } from "@hugeicons/core-free-icons";
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    setMounted(true);
+  }, []);
+
+  return (
+    <header className="flex border-b p-5 justify-between">
+      <Link href="/" className="block">
+        <Image src="/logo.svg" width={100} height={100} alt="SpendWise" />
+      </Link>
+      <div className="flex flex-row items-center justify-center gap-2">
+        {mounted ? (
+          <>
+            <HugeiconsIcon icon={Sun01Icon} size={16} />
+            <Switch
+              className="cursor-pointer"
+              checked={theme === "dark"}
+              onCheckedChange={(checked) =>
+                setTheme(checked ? "dark" : "light")
+              }
+            />
+            <HugeiconsIcon icon={Moon02Icon} size={16} />
+          </>
+        ) : (
+          <div className="w-20" /> /* Placeholder to prevent layout shift */
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
